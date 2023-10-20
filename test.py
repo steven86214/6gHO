938df065a8d1de8f2a8e56cd784f92ab41b6c54c
steven86214 <steven86214@gmail.com>
2023-10-20 17:10:36 +0800

    Update test.py

--- test.py
+++ test.py
@@ -8,6 +8,9 @@ import torch
 # else:
 #     print("Device set to : cpu")
 t = [[1,2,3],[4,5,6]]
+t1 = [[7,8,9],[10,11,12]]
 g = torch.FloatTensor(t)
+g1 = torch.FloatTensor(t1)
+h = torch.cat((g,g1),0)
 #.to(ppo_device)
 print(g)
