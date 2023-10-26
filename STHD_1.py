fd1768e368d1c3bd3c3c146f07f3859f12fd4107
steven86214 <steven86214@gmail.com>
2023-10-26 13:02:40 +0800

    Update STHD_1.py

--- STHD_1.py
+++ STHD_1.py
@@ -1002,6 +1002,8 @@ def Rewards_cal(obj_UE, num_of_UE, num_of_cell_block,servering_st,L, connect_tab
             # reward = -10
             BR= (servering_st[i]-L)/L
             reward = -(BR/v_table[time][st_ID])*1000
+            print("bR",BR)
+            print("v_table[time][st_ID]",v_table[time][st_ID])
             print("overload and the reword = ",reward)
             
         elif num_of_cell_block[st_ID]==0:   
