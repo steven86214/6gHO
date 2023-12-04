3d06842fcfc5739067279cf99116ce077cff8150
steven86214 <steven86214@gmail.com>
2023-12-04 10:35:24 +0800

    Update PPO_TF_6-60_DQN.py

--- PPO_TF_6-60_DQN.py
+++ PPO_TF_6-60_DQN.py
@@ -331,11 +331,11 @@ s= 2
 #s=3, Random_ST
 #s=4, graph
 num_of_channel =5
-e_greedy =0.1
+e_greedy =0
 #e_greedy = 1  #always random
 # training part
 time_step=0
-max_training_timesteps=200
+max_training_timesteps=5
 update_timestep=1
 save_model_freq=10
 
@@ -364,7 +364,7 @@ while time_step <= max_training_timesteps:
     # if time_step == 100:
     #     e_greedy-=0.1
     if time_step == 150:
-        e_greedy-=0.1
+        e_greedy = 0
     print('random'+str(Rand))
     Dual_Connect.sim(algo=PPO_TF_HO_Algo,algo_name='PPO_TF_HO_Algo',timeslot=Timeslot,                    carrier_bandwidth=Carrier_bandwidth,
                     num_of_UE=Num_of_UE,demand=Demand,num_of_st=Num_of_st,\
